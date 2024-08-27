bb14d71256b7036b9b07b55b36e37feb36fbe245
iselacavanzon <isellacavanzon@gmail.com>
2024-08-27 14:26:41 -0600

    first commit

---
+++ numero_mayor.py
@@ -0,0 +1,12 @@
+#leer y determinar que numero es mayor
+
+a = int(input("Ingresa un numero: "))
+b = int(input("Ingresa otro numero: "))
+
+if a==b :
+    print("Los numeros son iguales")
+else:
+    if a > b :
+     print(f"El numero mayor es: {a}")  
+    else:
+       print(f"El numero mayor es: {b}") 
---
+++ suma_nenteros.py
@@ -0,0 +1,10 @@
+#suma de numeros enteros
+
+print("Ingresa el valor final (n):")
+a = int(input())
+b = 0
+for i in range(1,a+1):
+    print(i)
+    b = b+i
+print("El valor de la suma es: ",b)
+    
